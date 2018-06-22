bdf6ea9a987c6f396bc5a2f56ba543902a753d8f
Richard Paleczny <palecznyr@gmail.com>
2018-06-21 23:27:55 -0600

    Completed Objective1Lab3.

---
+++ Labs/Objective1Lab3.java
@@ -0,0 +1,8 @@
+public class Objective1Lab3 {
+  public static void main(String[] args) {
+
+    System.out.println("\nName : Richard Paleczny\nAge : 26 (27 on Sep. 23)" +
+    "\nState : Virginia");
+
+  }
+}
