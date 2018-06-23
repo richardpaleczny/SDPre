8cf26a1989b9eddb4caffd958c6632fe94ab4adf
Richard Paleczny <palecznyr@gmail.com>
2018-06-23 14:53:49 -0600

    Completed Objective2Lab3.

---
+++ Labs/Objective2Lab3.java
@@ -0,0 +1,12 @@
+public class Objective2Lab3 {
+  public static void main(String[] args) {
+    String food1, food2, food3;
+
+    food1 = "Pizza";
+    food2 = "Alfredo";
+    food3 = "Pot Roast";
+
+    System.out.println("My Favorite Foods Are:\n" + "1. " + food1 +
+    "\n2. " + food2 + "\n3. " + food3);
+  }
+}
