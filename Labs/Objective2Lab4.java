60585557c17acec2d8cd5077162b20c38f222ae3
Richard Paleczny <palecznyr@gmail.com>
2018-06-23 14:56:36 -0600

    Completed Objective2Lab4.

---
+++ Labs/Objective2Lab4.java
@@ -0,0 +1,13 @@
+public class Objective2Lab4 {
+  public static void main(String[] args) {
+    double num1, num2, num3, average;
+
+    num1 = 12.3;
+    num2 = 10.5;
+    num3 = 18.0;
+
+    average = (num1 + num2 + num3) / 3;
+
+    System.out.println(average);
+  }
+}
