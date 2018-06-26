3ae0d8c2afa5d450d75e5ddd63d05b5939aedc8a
Richard Paleczny <palecznyr@gmail.com>
2018-06-26 01:19:58 -0600

    Completed Objective6 Labs: 1 - 5.

---
+++ Labs/Objective6Lab2.java
@@ -0,0 +1,15 @@
+public class Objective6Lab2 {
+  public static void main(String[] args) {
+    int counter = 0;
+
+    while(counter <= 20) {
+       if (counter <= 19) {
+        counter++;
+        System.out.println(counter);
+      } else if (counter == 20) {
+        counter++;
+      }
+      
+    }
+  }
+}
---
+++ Labs/Objective6Lab4.java
@@ -0,0 +1,12 @@
+public class Objective6Lab4 {
+  public static void main(String[] args) {
+    int current = 0, i = 0;
+    
+    while(i < 21) {
+      current += i;
+      i++;
+    }
+
+    System.out.println(current);
+  }
+}
